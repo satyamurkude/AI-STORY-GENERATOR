5101d36a782edd860370980085319301582f6ef6
Satyam Urkude <111609035+satyamurkude@users.noreply.github.com>
2025-08-25 20:40:03 +0530

    dd

--- Frontend/react/vite.config.js
+++ Frontend/react/vite.config.js
@@ -1,24 +1,24 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  // loadEnv returns all env vars for the given mode as strings
-  const env = loadEnv(mode, process.cwd(), "VITE_"); // only loads vars starting with VITE_
+// https://vite.dev/config/
+export default defineConfig((command, mode) => {
+  const env = loadEnv(mode, process.cwd(), "")
 
-  const debug = env.VITE_DEBUG === "true"; // convert to boolean
+  console.log(env.VITE_DEBUG)
 
   return {
     plugins: [react()],
-    server: debug
-      ? {
-          proxy: {
-            "/api": {
-              target: "http://localhost:8000",
-              changeOrigin: true,
-              secure: false,
-            },
-          },
+    server: {
+    ...(env.VITE_DEBUG === "true" && {
+      proxy: {
+        "/api": {
+          target: "http://localhost:8000",
+          changeOrigin: true,
+          secure: false
         }
-      : {},
-  };
-});
+      }
+    }
+    )}
+  }
+})
